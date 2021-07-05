c1243842a0ae9e9d3b48a6bdf2d1e4433af27047
break-head <zkwks0130@gmail.com>
2021-07-05 15:21:21 +0900

    처음시작

---
+++ class/pages/boards/new/index.js
@@ -0,0 +1,3 @@
+export default function BoardPage() {
+  return <div>게시판 등록 화면입니다.</div>;
+}
---
+++ class/styles/Home.styles.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.h1`
+  color: black;
+`;
+export const Name = styled.div`
+  font-size: 10px;
+`;
+
+export const Wrapper = styled.div``;
+
+export const PassWord = styled.div`
+  font-size: 10px;
+`;
+
+export const NameInput = styled.input``;
+export const PassWordInput = styled.input``;
