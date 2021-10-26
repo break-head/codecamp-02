502e3f3f1f0d8b274ce1e54c98c26f01042c66c7
break-head <zkwks0130@gmail.com>
2021-10-27 03:05:24 +0900

    fix

---
+++ freeboard_frontend/_next/server/pages/_app.js
@@ -0,0 +1,137 @@
+(function() {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 2372:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@ant-design/icons");;
+
+/***/ }),
+
+/***/ 8074:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@apollo/client");;
+
+/***/ }),
+
+/***/ 3245:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@apollo/client/link/error");;
+
+/***/ }),
+
+/***/ 6177:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@emotion/styled");;
+
+/***/ }),
+
+/***/ 8300:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@sentry/nextjs");;
+
+/***/ }),
+
+/***/ 953:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("antd");;
+
+/***/ }),
+
+/***/ 3483:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("apollo-upload-client");;
+
+/***/ }),
+
+/***/ 9421:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("firebase/app");;
+
+/***/ }),
+
+/***/ 9714:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("firebase/firestore");;
+
+/***/ }),
+
+/***/ 7435:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("graphql-request");;
+
+/***/ }),
+
+/***/ 701:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/head");;
+
+/***/ }),
+
+/***/ 6731:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/router");;
+
+/***/ }),
+
+/***/ 9297:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ }),
+
+/***/ 9080:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react-slick");;
+
+/***/ }),
+
+/***/ 5282:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react/jsx-runtime");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = __webpack_require__.X(0, [252], function() { return __webpack_exec__(7252); });
+module.exports = __webpack_exports__;
+
+})();
